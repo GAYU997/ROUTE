4643b6ba0411a3a75664b0976830a210040f9648
Gayathri.v <gayu9158@gmail.com>
2020-07-02 15:28:07 +0530

    commit

---
+++ app/route.js
@@ -0,0 +1,11 @@
+var app = angular.module("window", ["ngRoute"]);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/menu1", {
+    templateUrl : "templates/menu1.html"
+  })
+  .when("/menu2", {
+    templateUrl : "templates/menu2.html",
+  });
+  
+});
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<script src="http://ajax.googleapis.com/ajax/libs/angular/1.6.9/angular.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/angular/1.6.9/angular.route.js"></script>
+<script src="app/route.js"></script>
+<body ng-app="window"><h1>WELCOME</h1>
+
+<a href="#!menu1">MENU1</a>
+<a href="#!menu2">MENU2</a>
+
+<div class="ng-view"></div>
+
+</body>
+</html>
