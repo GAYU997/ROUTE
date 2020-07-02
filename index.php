57787d157fa35811da963d1fbe025756dad5d6ef
GAYU997 <67303008+GAYU997@users.noreply.github.com>
2020-07-02 22:01:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
-<html>
-<script src="http://ajax.googleapis.com/ajax/libs/angular/1.6.9/angular.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/angular/1.6.9/angular.route.js"></script>
+<html lang="en-US">
+<head>
+
+<meta charset="utf-g">
+<meta http-equiv="X-UX-Compatible" content="IE-edge">
+<meta name="viewport" content="width-device-width,initial-scale=1">
+<title>WEBSITE</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
 <script src="app/route.js"></script>
-<body ng-app="window"><h1>WELCOME</h1>
+<script src="app/controllers/postController.js"></script>
 
-<a href="#!menu1">MENU1</a>
-<a href="#!menu2">MENU2</a>
-
-<div class="ng-view"></div>
-
-</body>
+<body ng-app="sample">
+ <center></br>
+ <h1><b>WELCOME GUEST..!!</b></h1>
+ <a href="/AngularForm">HOME</a>
+ <a href="#!menu1">MENU1</a>
+ <div class="ng-view"></div></br>
+ <center>
+ </body>
 </html>
